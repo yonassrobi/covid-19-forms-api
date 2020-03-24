d0976566a8fa553685c16ca236b76849b6849862
Mike Endale <mike@blencorp.com>
2020-03-24 16:08:45 -0400

    Updated the travelers payload.

--- handler.js
+++ handler.js
@@ -4,6 +4,8 @@ const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
 const uuid = require('uuid/v4')
 
 const postsTable = process.env.POSTS_TABLE
+const travelersTable = process.env.TRAVELERS_TABLE
+
 // Create a response
 function response(statusCode, message) {
   return {
@@ -69,7 +71,7 @@ module.exports.getPost = (event, context, callback) => {
 
   const params = {
     Key: {
-      id: id
+      ID: id
     },
     TableName: postsTable
   }
@@ -91,7 +93,7 @@ module.exports.updatePost = (event, context, callback) => {
 
   const params = {
     Key: {
-      id: id
+      ID: id
     },
     TableName: postsTable,
     ConditionExpression: 'attribute_exists(id)',
@@ -118,9 +120,125 @@ module.exports.deletePost = (event, context, callback) => {
   const id = event.pathParameters.id
   const params = {
     Key: {
-      id: id
+      ID: id
     },
     TableName: postsTable
+  }
+  return db
+    .delete(params)
+    .promise()
+    .then(() =>
+      callback(null, response(200, { message: 'Post deleted successfully' }))
+    )
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
+
+// Travelers
+// Create a post
+module.exports.createTraveler = (event, context, callback) => {
+  const reqBody = JSON.parse(event.body)
+
+  const post = {
+    uuid: uuid(),
+    ...reqBody
+  }
+
+  return db
+    .put({
+      TableName: travelersTable,
+      Item: post
+    })
+    .promise()
+    .then(() => {
+      callback(null, response(201, post))
+    })
+    .catch(err => response(null, response(err.statusCode, err)))
+}
+// Get all posts
+module.exports.getAllTravelers = (event, context, callback) => {
+  return db
+    .scan({
+      TableName: travelersTable
+    })
+    .promise()
+    .then(res => {
+      callback(null, response(200, res.Items.sort(sortByDate)))
+    })
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
+// Get number of posts
+module.exports.getTravelers = (event, context, callback) => {
+  const numberOfPosts = event.pathParameters.number
+  const params = {
+    TableName: travelersTable,
+    Limit: numberOfPosts
+  }
+  return db
+    .scan(params)
+    .promise()
+    .then(res => {
+      callback(null, response(200, res.Items.sort(sortByDate)))
+    })
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
+// Get a single post
+module.exports.getTraveler = (event, context, callback) => {
+  const id = event.pathParameters.id
+
+  const params = {
+    Key: {
+      ID: id
+    },
+    TableName: travelersTable
+  }
+
+  return db
+    .get(params)
+    .promise()
+    .then(res => {
+      if (res.Item) callback(null, response(200, res.Item))
+      else callback(null, response(404, { error: 'Post not found' }))
+    })
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
+// Update a post
+module.exports.updateTraveler = (event, context, callback) => {
+  const id = event.pathParameters.id
+  const reqBody = JSON.parse(event.body)
+  const { body, title } = reqBody
+
+  const params = {
+    Key: {
+      ID: id
+    },
+    TableName: travelersTable,
+    ConditionExpression: 'attribute_exists(id)',
+    UpdateExpression: 'SET title = :title, body = :body',
+    ExpressionAttributeValues: {
+      ':title': title,
+      ':body': body
+    },
+    ReturnValues: 'ALL_NEW'
+  }
+  console.log('Updating')
+
+  return db
+    .update(params)
+    .promise()
+    .then(res => {
+      console.log(res)
+      callback(null, response(200, res.Attributes))
+    })
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
+// Delete a post
+module.exports.deleteTraveler = (event, context, callback) => {
+  const id = event.pathParameters.id
+  const params = {
+    Key: {
+      ID: id
+    },
+    TableName: travelersTable
   }
   return db
     .delete(params)

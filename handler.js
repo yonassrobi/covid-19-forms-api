4948221fbf7b2883587df338a1685e1b64ec0aac
Mike Endale <mike@blencorp.com>
2020-03-24 14:07:51 -0400

    Updated the endpoints to use the forms API.

--- handler.js
+++ handler.js
@@ -1,46 +1,29 @@
-'use strict';
-const AWS = require('aws-sdk');
-const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
-const uuid = require('uuid/v4');
+'use strict'
+const AWS = require('aws-sdk')
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
+const uuid = require('uuid/v4')
 
-const postsTable = process.env.POSTS_TABLE;
+const postsTable = process.env.POSTS_TABLE
 // Create a response
 function response(statusCode, message) {
   return {
     statusCode: statusCode,
     body: JSON.stringify(message)
-  };
+  }
 }
 function sortByDate(a, b) {
   if (a.createdAt > b.createdAt) {
-    return -1;
-  } else return 1;
+    return -1
+  } else return 1
 }
 // Create a post
 module.exports.createPost = (event, context, callback) => {
-  const reqBody = JSON.parse(event.body);
-
-  if (
-    !reqBody.title ||
-    reqBody.title.trim() === '' ||
-    !reqBody.body ||
-    reqBody.body.trim() === ''
-  ) {
-    return callback(
-      null,
-      response(400, {
-        error: 'Post must have a title and body and they must not be empty'
-      })
-    );
-  }
+  const reqBody = JSON.parse(event.body)
 
   const post = {
-    id: uuid(),
-    createdAt: new Date().toISOString(),
-    userId: 1,
-    title: reqBody.title,
-    body: reqBody.body
-  };
+    uuid: uuid(),
+    ...reqBody
+  }
 
   return db
     .put({
@@ -49,10 +32,10 @@ module.exports.createPost = (event, context, callback) => {
     })
     .promise()
     .then(() => {
-      callback(null, response(201, post));
+      callback(null, response(201, post))
     })
-    .catch((err) => response(null, response(err.statusCode, err)));
-};
+    .catch(err => response(null, response(err.statusCode, err)))
+}
 // Get all posts
 module.exports.getAllPosts = (event, context, callback) => {
   return db
@@ -60,51 +43,51 @@ module.exports.getAllPosts = (event, context, callback) => {
       TableName: postsTable
     })
     .promise()
-    .then((res) => {
-      callback(null, response(200, res.Items.sort(sortByDate)));
+    .then(res => {
+      callback(null, response(200, res.Items.sort(sortByDate)))
     })
-    .catch((err) => callback(null, response(err.statusCode, err)));
-};
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
 // Get number of posts
 module.exports.getPosts = (event, context, callback) => {
-  const numberOfPosts = event.pathParameters.number;
+  const numberOfPosts = event.pathParameters.number
   const params = {
     TableName: postsTable,
     Limit: numberOfPosts
-  };
+  }
   return db
     .scan(params)
     .promise()
-    .then((res) => {
-      callback(null, response(200, res.Items.sort(sortByDate)));
+    .then(res => {
+      callback(null, response(200, res.Items.sort(sortByDate)))
     })
-    .catch((err) => callback(null, response(err.statusCode, err)));
-};
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
 // Get a single post
 module.exports.getPost = (event, context, callback) => {
-  const id = event.pathParameters.id;
+  const id = event.pathParameters.id
 
   const params = {
     Key: {
       id: id
     },
     TableName: postsTable
-  };
+  }
 
   return db
     .get(params)
     .promise()
-    .then((res) => {
-      if (res.Item) callback(null, response(200, res.Item));
-      else callback(null, response(404, { error: 'Post not found' }));
+    .then(res => {
+      if (res.Item) callback(null, response(200, res.Item))
+      else callback(null, response(404, { error: 'Post not found' }))
     })
-    .catch((err) => callback(null, response(err.statusCode, err)));
-};
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
 // Update a post
 module.exports.updatePost = (event, context, callback) => {
-  const id = event.pathParameters.id;
-  const reqBody = JSON.parse(event.body);
-  const { body, title } = reqBody;
+  const id = event.pathParameters.id
+  const reqBody = JSON.parse(event.body)
+  const { body, title } = reqBody
 
   const params = {
     Key: {
@@ -118,32 +101,32 @@ module.exports.updatePost = (event, context, callback) => {
       ':body': body
     },
     ReturnValues: 'ALL_NEW'
-  };
-  console.log('Updating');
+  }
+  console.log('Updating')
 
   return db
     .update(params)
     .promise()
-    .then((res) => {
-      console.log(res);
-      callback(null, response(200, res.Attributes));
+    .then(res => {
+      console.log(res)
+      callback(null, response(200, res.Attributes))
     })
-    .catch((err) => callback(null, response(err.statusCode, err)));
-};
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
 // Delete a post
 module.exports.deletePost = (event, context, callback) => {
-  const id = event.pathParameters.id;
+  const id = event.pathParameters.id
   const params = {
     Key: {
       id: id
     },
     TableName: postsTable
-  };
+  }
   return db
     .delete(params)
     .promise()
     .then(() =>
       callback(null, response(200, { message: 'Post deleted successfully' }))
     )
-    .catch((err) => callback(null, response(err.statusCode, err)));
-};
+    .catch(err => callback(null, response(err.statusCode, err)))
+}
